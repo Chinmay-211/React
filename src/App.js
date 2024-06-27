f296f34b543291ad7d8286ce1ded1325da226db7
Chinmay-211 <chinmaynaik211@gmail.com>
2024-06-27 18:15:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,65 +1,62 @@
-
-import React,{useState} from 'react';
- 
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbargit';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//update
 function App() {
-const [mode,setmode]=useState('light');
-const [text,setText]=useState('black');
-const [alert,setalert]=useState(null);
+  const [mode, setmode] = useState('light');
+  const [text, setText] = useState('black');
+  const [alert, setalert] = useState(null);
 
-const showAlert=(message,type)=>{
-  setalert({
-    msg:message,
-    type:type
-  });
-  setTimeout(() => {
-   setalert(null);
-  }, 1500);
-
-
-}
-const togle=()=>{
-if(mode==='light')
-  {
-    setmode('dark');
-    document.body.style.backgroundColor='black';
-    showAlert("Dark mode enabled","Success")
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
   }
-else{
-  setmode('light');
-  document.body.style.backgroundColor='white';
-  showAlert("light mode enabled","Success")
-} 
-}
-const textTogel=()=>
-  {
-    if(text==='white')
-      {
-        setText('black');
-      }
-    else{
+
+  const togle = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode enabled", "Success");
+    } else {
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode enabled", "Success");
+    }
+  }
+
+  const textTogel = () => {
+    if (text === 'white') {
+      setText('black');
+    } else {
       setText('white');
     }
   }
 
-
-
-
-
-
   return (
-   <>
-  
-   <Navbar mode={mode} togle={togle} text={textTogel}/>
-   <Alert alert={alert}/>
-   <div className="container">
-   <TextForm heading="enter text" mode={mode} showAlert={showAlert}/>
-   </div>
-  
-   </>
+    <>
+      <Router>
+        <Navbar mode={mode} togle={togle} text={textTogel} />
+        <Alert alert={alert} />
+        <div className="container">
+          <switch>
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter text" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+          </switch>
+          
+        </div>
+      </Router>
+    </>
   );
 }
 
